4ddd5bc894585b2f6d43ec2ee913804e3f07c411
Jon Redmond <jredmond@sapient.com>
2018-09-12 15:08:20 +0100

    basic html elements, form, submit action, list of names

--- server/index.js
+++ server/index.js
@@ -3,4 +3,6 @@ const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
 
+app.post('/submit', (req, res) => console.log(req) && res.send(JSON.stringify(req.params)));
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
