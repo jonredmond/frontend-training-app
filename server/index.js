4e8f8817761e7e3410d1c4ea8111205ac792a375
Jon Redmond <jredmond@sapient.com>
2018-09-12 16:17:50 +0100

    start for css assignment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
-const app = require('express')();
+const express = require('express');
 const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
+const app = express();
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, '../sass'),
+  dest: path.join(__dirname, '../client/styles'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/styles'
+}));
+
+app.use(express.static('client'));
 
 app.post('/submit', (req, res) => console.log(req) && res.send(JSON.stringify(req.params)));
 
